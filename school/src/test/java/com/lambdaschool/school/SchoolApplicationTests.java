f7aea28f693c75a9da70fb3e75840378136731fd
danpatrickoneill <dan47573@gmail.com>
2019-05-13 18:01:59 -0400

    Initial commit - successfully wrote and ran first three tests

--- school/src/test/java/com/lambdaschool/school/SchoolApplicationTests.java
+++ school/src/test/java/com/lambdaschool/school/SchoolApplicationTests.java
@@ -1,18 +1,55 @@
 package com.lambdaschool.school;
 
+import com.lambdaschool.school.service.CourseService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.Assert.*;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SchoolApplication.class)
 public class SchoolApplicationTests
 {
+    @Autowired
+    private CourseService courseService;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void contextLoads()
     {
+    }
+
+    @Test
+    public void findCourseById()
+    {
+        assertNotNull(courseService.findCourseById(1));
+    }
+
+    @Test
+    public void deleteFound()
+    {
+        courseService.delete(1);
+        assertEquals(5, courseService.findAll().size());
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void deleteNotFound()
+    {
+        courseService.delete(100);
+        assertEquals(6, courseService.findAll().size());
     }
 
 }
